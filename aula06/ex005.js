15936e63a365c89bf82476cec2f1d60919a67ae5
Caique fernandes <kakaifernandes952@gmail.com>
2025-06-17 21:33:58 -0300

    Dando comandos com JavaScript

---
+++ aula06/ex005.js
@@ -0,0 +1,8 @@
+var p1 = window.document.getElementsByTagName('p')[1]
+window.document.write('Está escrito assim: ' + p1.innerText)
+p1.style.color = 'black'
+var corpo = document.body
+corpo.style.background = 'red'
+var d = window.document.getElementById('msg')
+d.innerText = 'Estou aguardando...'
+d.style.background = 'green'
