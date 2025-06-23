f05192f6f245f017f8647fe1963ca3af019aea33
Caique fernandes <kakaifernandes952@gmail.com>
2025-06-22 22:23:00 -0300

    if e else

---
+++ aula011/aula009.js
@@ -0,0 +1,6 @@
+var pais = 'França'
+if (pais == 'Brasil') {
+    console.log(`você é brasileiro`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A sua velociade é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade.Multado `)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
