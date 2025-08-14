4f236b34796dd36b9cd6cb2936aff83ac3a2a07b
Caique fernandes <kakaifernandes952@gmail.com>
2025-08-14 20:24:20 -0300

    código de comandos
     DO e WHILE

---
+++ aula016/ambiente.js
@@ -0,0 +1,29 @@
+/* Este tipoi de códogo esecuta primeiro */
+var c = 1
+do {
+    console.log(`Passo ${c}`)/*<= Esecuta*/
+    c++ /*<= Esecuta*/
+} while (c <= 22) /*<= Testa */
+/*E tesa no final*/
+
+
+
+/*
+    Este código testa primeiro e esecuta depois 
+
+var c = 1
+while (c <= 22) {      <= O teste         
+    console.log(`Passo ${c}`)   <= Oque esecuta
+    c++     <= Oque esecuta
+}
+    */
+
+/*
+Metodo errado!
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
---
+++ aula016/node_cda514951612c.tmp.js
@@ -0,0 +1,20 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+}
+
+
+
+
+
+
+
+/*
+Metodo errado!
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
